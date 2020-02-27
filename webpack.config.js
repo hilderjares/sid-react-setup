92e18da0fb0df5e561a886717fdf1cd23436448e
hilderjares <hider1195@gmail.com>
2020-02-27 12:04:16 -0300

    webackpack settings logs

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin')
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
         path: __dirname + '/build', filename: 'bundle.js'
+    },
+    devServer: {
+        compress: true,
+        port: 9000,
+        quiet: true,
     },
     module: {
         rules: [{
@@ -18,7 +25,10 @@ module.exports = {
             ]
         }]
     },
+    devtool: 'cheap-module-source-map',
     plugins: [
-        new HtmlWebpackPlugin({ template: './public/index.html' })
+        new HtmlWebpackPlugin({ template: './public/index.html' }),
+        new FriendlyErrorsWebpackPlugin(),
+        new ErrorOverlayPlugin()    
     ]
 }
